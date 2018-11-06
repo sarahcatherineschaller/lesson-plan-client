2a2f66e6f8e79e017c63a28570505414b7e7d1bb
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-06 18:58:15 -0500

    add links to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
 	return (
 		<div>
-			Home
+			<h1>Welcome To Your Classroom</h1>{' '}
+			<Link to={`/lessons`}>Lesson Plans</Link>{' '}
+			<Link to={`/lessons/new`}>Create A New Lesson Plan</Link>
 		</div>
 	)
 }
--- src/containers/Lesson.js
+++ src/containers/Lesson.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import LessonCard from '../components/LessonCard';
+import { getLesson } from '../actions/lessons';
+import LessonForm from './LessonForm'
+import Lessons from './Lessons';
 
 class Lesson extends Component {
+
+	componentDidMount() {
+		this.props.getLesson(this.props.match.params.recipeId)
+	}
 	render() {
+		let lesson = this.props.lesson[0]
 		return(
 			<div>
-				
+				<h1>Lesson</h1>
 			</div>
 		)
 	}
 }
 
-const mapStateToProps = (state, ownProps) => {
-	const lesson = state.lessons.find(lesson => {
-		return lesson.id == ownProps.match.params.lessonId
+const mapStateToProps = (state) => {
+	return({
+		lesson: state.lessonsReducer
 	})
-
-	return {
-		lesson
-	}
 }
 
-export default connect(mapStateToProps)(Lesson);
+export default connect(mapStateToProps, { getLesson })(Lesson);
