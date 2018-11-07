a29ee4037e70d8f5c635a7be3aba4ac88385d90d
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-06 21:57:52 -0500

    add styling to form

--- src/containers/LessonForm.js
+++ src/containers/LessonForm.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createLesson } from '../actions/lessons';
 import { updateLessonFormData } from '../actions/lessonForm';
 import Lessons from './Lessons';
+import { Col, Button, Form, Label, Input, FormGroup, FormText } from 'reactstrap';
 
 class LessonForm extends Component {
 
@@ -23,29 +24,47 @@ class LessonForm extends Component {
 		const { title, grade_level, subject, total_time, objective, materials, summary, other } = this.props.lessonFormData
 
 		return(
-			<div>
-				<form onSubmit={ (event) => this.handleSubmit(event) }>
-				<p>
-					<label>Title</label>
-					<input type="text" onChange={ event => this.handleChange(event) } name="title" value={ title } />
-					<label>Grade Level</label>
-					<input type="text" onChange={ event => this.handleChange(event) } name="grade_level" value={ grade_level } />
-					<label>Subject</label>
-					<input type="text" onChange={ event => this.handleChange(event) } name="subject" value={ subject } />
-					<label>Total Time</label>
-					<input type="text" onChange={ event => this.handleChange(event) } name="total_time" value={ total_time } />
-					<label>Objective</label>
-					<textarea onChange={ event => this.handleChange(event) } name="objective" value={ objective } />
-					<label>Materials</label>
-					<textarea onChange={ event => this.handleChange(event) } name="materials" value={ materials } />
-					<label>Summary</label>
-					<textarea onChange={ event => this.handleChange(event) } name="summary" value={ summary } />
-					<label>Other</label>
-					<textarea onChange={ event => this.handleChange(event) } name="other" value={ other } />
-				</p>
-				<input className="newLessonButton" type="submit" />
-				</form>
-			</div>
+				<Col sm="12" md={{ size: 6, offset:3 }}>
+				<Form onSubmit={ (event) => this.handleSubmit(event) }>
+				<FormGroup>
+					<Label>Title</Label>
+					<Input type="text" onChange={ event => this.handleChange(event) } name="title" value={ title } />
+		
+				</FormGroup>
+				<FormGroup>
+					<Label>Grade Level</Label>
+					<Input type="text" onChange={ event => this.handleChange(event) } name="grade_level" value={ grade_level } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Subject</Label>
+					<Input type="text" onChange={ event => this.handleChange(event) } name="subject" value={ subject } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Total Time</Label>
+					<Input type="text" onChange={ event => this.handleChange(event) } name="total_time" value={ total_time } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Objective</Label>
+					<Input type="textea" onChange={ event => this.handleChange(event) } name="objective" value={ objective } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Materials</Label>
+					<Input type="textarea" onChange={ event => this.handleChange(event) } name="materials" value={ materials } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Summary</Label>
+					<Input type="textarea" onChange={ event => this.handleChange(event) } name="summary" value={ summary } />
+				</FormGroup>
+				<FormGroup>
+					<Label>Other</Label>
+					<Input type="textarea" onChange={ event => this.handleChange(event) } name="other" value={ other } />
+				</FormGroup>
+				<Button color="primary">
+					Submit
+				</Button>
+				
+			</Form>
+			</Col>
 		)
 	}
 }
