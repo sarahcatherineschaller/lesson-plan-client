7f922460bfc810c950aae10470f98bd33f992bfa
Sarah Schaller <sarahcschaller@gmail.com>
2018-11-12 13:41:30 -0500

    lesson likes reducer

--- src/reducers/lessonsReducer.js
+++ src/reducers/lessonsReducer.js
@@ -8,6 +8,14 @@ const lessonsReducer = (state = [], action) => {
 			return state.concat(action.lesson)
 		case 'REMOVE_LESSON':
 			return state.filter(lesson => lesson.id !== action.lessonId);
+		case 'UPDATE_LIKE_COUNTER':
+			return state.map(lesson => {
+				if (lesson.id === action.lesson.id) {
+					return action.lesson 
+				} else {
+					return lesson
+				}
+			})
 		// case 'EDIT_LESSON':
 		// 	return state.map(lesson => {
 		// 		if (lesson.id === action.lesson.id) {
